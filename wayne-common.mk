2e97ae340456a6dcead4d3fb4a0398a588b7df00
rpbritton <git@rbritton.dev>
2022-03-22 14:36:36 -0400

    wayne-common: PE bring up

--- wayne-common.mk
+++ wayne-common.mk
@@ -73,7 +73,7 @@ PRODUCT_COPY_FILES += \
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(DEVICE_PATH)/overlay \
-    $(DEVICE_PATH)/overlay-lineage
+    $(DEVICE_PATH)/overlay-pe
 
 # Permissions
 PRODUCT_COPY_FILES += \
